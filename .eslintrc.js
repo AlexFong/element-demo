6abe84ff1398b7fac22a5cac59cb727af7cec96b
AlexFong <361542446@qq.com>
2021-04-24 23:46:02 +0800

    引入element-ui

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,15 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
+    // "规则名": [规则值, 规则配置]
+    // "off"或者0    //关闭规则关闭
+    // "warn"或者1    //在打开的规则作为警告（不影响退出代码）
+    // "error"或者2    //把规则作为一个错误（退出代码触发时为1）
+
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "semi": [0],  // 关闭语句强制分号结尾
+    "space-before-function-paren": [0, "always"],//函数定义时括号前面要不要有空格
   },
   overrides: [
     {
